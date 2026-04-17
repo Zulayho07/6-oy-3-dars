378a65f2c20bc1d14e2cdddadf8a665e94c7af07
Zulayho <abduqaxxorovazulayho@gmail.com>
2026-04-18 01:31:16 +0500

    4-dars homework

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import about
-
+from .views import (about, home, news_by_category, news_detail)
 
 urlpatterns=[
-    path('', about, name='about'),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('news/<int:news_id>/', news_detail, name='news_detail'),
+    path('categories/<int:category_id>/', news_by_category, name='news_by_category'),
 ]
--- project/main/views.py
+++ project/main/views.py
@@ -1,11 +1,48 @@
 from django.shortcuts import render
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
+from .models import Category, News
+
+
+def home(request: HttpRequest):
+    categories=Category.objects.all()
+    news=News.objects.all()
+
+    context={
+        'categories':categories,
+        'news':news,
+        'title': 'Yangiliklar'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def news_detail(request: HttpRequest, news_id):
+    news=News.objects.get(id=news_id)
+    context={
+        'news': news,
+    }
+
+    return render(request, 'main/detail.html', context)
+
+
+def news_by_category(request: HttpRequest, category_id):
+    categories=Category.objects.all()
+    category=Category.objects.get(id=category_id)
+    news=News.objects.filter(category=category_id)
+
+    context = {
+        'news': news,
+        'categories':categories,
+        'title': category
+    }
+
+    return render(request, 'main/index.html', context)
+
 
 def about(request: HttpRequest):
     data = {
         "company": "My Company",
-         "address": "Tashkent",
+        "address": "Tashkent",
         "phone": "+998901234567",
         "email": "info@gmail.com"
     }
